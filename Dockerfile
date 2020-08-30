bbf5fcaeff4763447ba62bbad8bdc87f61203e83
Pablo Morelli <pabmorelli@gmail.com>
2020-08-30 21:11:36 +0200

    added SSL certificates for scratch

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,11 @@
 FROM golang:1.15-alpine3.12 AS compiler
+RUN apk update && apk add --no-cache git ca-certificates && update-ca-certificates
 
 WORKDIR /builder
 ADD . ./
 RUN CGO_ENABLED=0 go build main.go
 
 FROM scratch
+COPY --from=builder /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
 COPY --from=compiler /builder/main /main
 ENTRYPOINT ["/main"]
--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"syscall"
 )
 
+// getEnv returns the value for an environment value, or a fallback if not found
 func getEnv(name string, fallback string) string {
 	value, ok := syscall.Getenv(name)
 	if !ok {
@@ -22,13 +23,15 @@ type SecretGetter struct {
 	GoogleCloudProject string
 }
 
+// GetSecret gets a secret either from environment variable or from GCP Secret Manager
 func (sg SecretGetter) GetSecret(name string, fallback string) string {
+	// If GCP project is not present, get value from environment variables
 	if sg.GoogleCloudProject == "" {
 		return getEnv(name, fallback)
 	}
 
+	// Get the token for the service account that runs the node pool
 	tokenUrl := "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token"
-
 	rq, err := http.NewRequest(http.MethodGet, tokenUrl, nil)
 	if err != nil {
 		fmt.Println(err)
@@ -58,6 +61,7 @@ func (sg SecretGetter) GetSecret(name string, fallback string) string {
 		return fallback
 	}
 
+	// Get the secret value using the access_token that we fetched above
 	secretUrl := fmt.Sprintf(
 		"https://content-secretmanager.googleapis.com/v1beta1/projects/%s/secrets/%s/versions/latest:access",
 		sg.GoogleCloudProject, name)
@@ -95,11 +99,13 @@ func (sg SecretGetter) GetSecret(name string, fallback string) string {
 		return fallback
 	}
 
+	// In case there is an error because of privileges or oauth scopes, return the fallback
 	if secretResponse.Error != 0 {
 		fmt.Println(fmt.Sprintf("error %d - status %s", secretResponse.Error, secretResponse.Status))
 		return fallback
 	}
 
+	// Secret Manager returns the secret on base64
 	data, err := base64.StdEncoding.DecodeString(secretResponse.Payload.Data)
 	if err != nil {
 		fmt.Println(err)
@@ -111,9 +117,11 @@ func (sg SecretGetter) GetSecret(name string, fallback string) string {
 
 func main() {
 
+	// Get GCP Project to know if we use environment variables or Secret Manager
 	googleCloudProject := getEnv("GCP_PROJECT", "")
 	secretGetter := SecretGetter{googleCloudProject}
 
+	// Set up the HTTP server for getting secrets
 	routes := http.NewServeMux()
 	routes.HandleFunc("/get-secret", getSecretHandler(secretGetter))
 	err := http.ListenAndServe(":8080", routes)
@@ -123,31 +131,37 @@ func main() {
 	}
 }
 
+// getSecretHandler gets the secret value according to the name sent on the header
 func getSecretHandler(secretGetter SecretGetter) http.HandlerFunc {
 	return func(w http.ResponseWriter, rq *http.Request) {
+		// Only work with GET requests
 		if rq.Method != http.MethodGet {
 			w.WriteHeader(http.StatusMethodNotAllowed)
 			return
 		}
 
+		// Fetch the secret name on the header
 		secretName := rq.Header.Get("secret")
 		if secretName == "" {
 			w.WriteHeader(http.StatusBadRequest)
 			return
 		}
 
+		// Create the struct definition for the response
 		bytes, err := json.Marshal(struct {
 			Name  string `json:"name"`
 			Value string `json:"value"`
 		}{
-			Name:  secretName,
-			Value: secretGetter.GetSecret(secretName, "default"),
+			Name: secretName,
+			// Use the secret getter to get the secret or the fallback
+			Value: secretGetter.GetSecret(secretName, fmt.Sprintf("default-for-%s", secretName)),
 		})
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
 
+		// Return the secret value
 		w.Header().Set("Content-Type", "application/json")
 		w.WriteHeader(http.StatusOK)
 		_, _ = w.Write(bytes)
